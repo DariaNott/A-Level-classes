021d51682502ab855622968ff8582e5414ed1113
Daria Ivanova <dasha.tas97@gmail.com>
2019-07-26 18:39:16 +0300

    Page Object homework

---
+++ POtestngTask/src/main/java/ua/dou/daria_ivanova/pages/Verifier.java
@@ -0,0 +1,29 @@
+package ua.dou.daria_ivanova.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+
+public class Verifier {
+    private WebDriver driver;
+
+    public Verifier (WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void verifyJobSearchResultsPresence (JobsPage page){
+        String xPath = "//a[contains(text(),'Больше вакансий')]";
+        page.waitVisibility(By.xpath(xPath));
+        Boolean moreVacancies = driver.findElement(By.xpath(xPath)).isDisplayed();
+        Assert.assertTrue(moreVacancies);
+    }
+
+    public void verifyJobSearchResultsAbsence (JobsPage page){
+        String xPath = "//div[contains(@id,'vacancyListId')]/a";
+        page.waitVisibility(By.xpath(xPath));
+        Boolean moreVacancies = driver.findElement(By.xpath(xPath)).isDisplayed();
+        Assert.assertTrue(moreVacancies);
+    }
+
+}
